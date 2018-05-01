9a508f5820cb79172516c73a90e476ba0281bc3c
Steven Spriggs <>
2018-05-01 15:51:19 -0400

    add sidekiq stuffs

--- lib/backgrounder/workers/store_asset_mixin.rb
+++ lib/backgrounder/workers/store_asset_mixin.rb
@@ -12,6 +12,7 @@ module CarrierWave
 
     module StoreAssetMixin
       include CarrierWave::Workers::Base
+      include ::Sidekiq::Status::Worker
 
       def self.included(base)
         base.extend CarrierWave::Workers::ClassMethods
@@ -20,13 +21,28 @@ module CarrierWave
       attr_reader :cache_path, :tmp_directory
 
       def perform(*args)
+        file = Upload.find(args[1])
+        at 10, "Performing"
+        status = Sidekiq::Status::get_all self.provider_job_id
+        ActionCable.server.broadcast 'activity_channel', {stage: "during", job: self.to_json, status: status.to_json, file: file.key}
+
         record = super(*args)
 
         if record && record.send(:"#{column}_tmp")
+
+          at 25, "Uploading"
+          status = Sidekiq::Status::get_all job.provider_job_id
+          ActionCable.server.broadcast 'activity_channel', {stage: "during", job: self.to_json, status: status.to_json, file: file.key}
+
           store_directories(record)
           record.send :"process_#{column}_upload=", true
           record.send :"#{column}_tmp=", nil
           record.send :"#{column}_processing=", false if record.respond_to?(:"#{column}_processing")
+
+          at 65, "Saving"
+          status = Sidekiq::Status::get_all job.provider_job_id
+          ActionCable.server.broadcast 'activity_channel', {stage: "during", job: self.to_json, status: status.to_json, file: file.key}
+
           open(cache_path) do |f|
             record.send :"#{column}=", f
           end
